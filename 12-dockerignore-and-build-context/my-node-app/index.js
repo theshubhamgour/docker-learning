9832b83699dd5d3302f0d92173ed8befcaf3d09e
Shubham Gour <iamtheshubhamgour@gmail.com>
2026-04-14 14:44:32 +0530

    add: node-app

---
+++ 12-dockerignore-and-build-context/my-node-app/index.js
@@ -0,0 +1,25 @@
+import http from "http";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, "message.txt");
+
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Error reading file");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/plain" });
+            res.end(data);
+        }
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
